f02cf163ebf58920545b893056f6d730f9203a01
Cal Clarke <>
2023-06-28 13:29:37 +0100

    Added main menu UI, buttons working

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField] Button _startGame;
+    [SerializeField] Button _exitGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startGame.onClick.AddListener(StartNewGame);
+        Debug.Log("buttonClicked");
+    }
+
+    private void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+ 
+}
