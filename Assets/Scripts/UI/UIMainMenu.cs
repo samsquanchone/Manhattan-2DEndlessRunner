c9438743ead6fa4c7b2ec5d99b29a149642c446a
Cal Clarke <>
2023-06-28 13:59:26 +0100

    Added death screen, buttons working

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 public class UIMainMenu : MonoBehaviour
 {
     [SerializeField] Button _startGame;
-    [SerializeField] Button _exitGame;
     // Start is called before the first frame update
     void Start()
     {
