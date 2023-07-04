0589d188d7351d01ea044330a94342ba657e86aa
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2023-07-04 13:56:08 +0100

    Merged whitehole stuff

--- Assets/Scripts/Objects/SpeedPickUp.cs
+++ Assets/Scripts/Objects/SpeedPickUp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public enum PickUpTypes { SPEED, HEALTH, WORMHOLE, WHITEHOLE}
+public enum PickUpTypes { SPEED, HEALTH, WORMHOLE, WHITEHOLE, SHIELD}
 
 
 public interface PickUpBase
